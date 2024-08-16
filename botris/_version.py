6381c00e20886c3b2199e0b8a177cf5116b942fe
Nathanael Lu <lunathanael@gmail.com>
2024-08-16 14:06:48 -0400

    Fixed cmake version checks

--- botris/_version.py
+++ botris/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.22.dev1'
-__version_tuple__ = version_tuple = (0, 1, 22, 'dev1')
+__version__ = version = '0.1.22.dev2+g4da4233.d20240816'
+__version_tuple__ = version_tuple = (0, 1, 22, 'dev2', 'g4da4233.d20240816')
