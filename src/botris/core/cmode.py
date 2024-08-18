d8ff2389fc9f1fda791e0a021758e6faa2b8e469
Nathanael Lu <lunathanael@gmail.com>
2024-08-18 15:08:40 -0400

    WIP: Added CBotris mode interface class

---
+++ src/botris/core/cmode.py
@@ -0,0 +1,67 @@
+from typing import TYPE_CHECKING
+
+from .cconstants import CspinType
+from botris._core.constants import Botris
+
+class CBotris:
+    """
+    A Python interface for the C++ Botris class, a class holding
+    the game options.
+
+    Attributes:
+    -----------
+    combo_table : List[int]
+        The combo table.
+    attack_table : List[int]
+        The attack table.
+    all_spin_bonus : int
+        The all-spin bonus.
+    pc_bonus : int
+        The perfect clear bonus.
+    b2b_bonus : int
+        The back-to-back bonus.
+
+    Methods:
+    --------
+    __init__(self)
+        Initialize the Botris class.
+    points(self, lines: int, spin: CspinType, pc: bool, combo: int, b2b: int) -> int
+        Calculate the points based on the number of lines cleared, spin type, and other factors.
+    """
+
+    combo_table: list[int] = Botris.combo_table
+    attack_table: list[int] = Botris.attack_table
+    all_spin_bonus: int = Botris.all_spin_bonus
+
+    pc_bonus: int = Botris.pc_bonus
+    b2b_bonus: int = Botris.b2b_bonus
+
+    def __init__(self):
+        """
+        Initialize the Botris class.
+        """
+        pass
+    
+    def points(self, lines: int, spin: CspinType, pc: bool, combo: int, b2b: int) -> int:
+        """
+        Calculate the points based on the number of lines cleared, spin type, and other factors.
+
+        Parameters:
+        -----------
+        lines : int
+            The number of lines cleared.
+        spin : CspinType
+            The spin type.
+        pc : bool
+            Whether the piece is a perfect clear.
+        combo : int (u32)
+            The current combo count.
+        b2b : int (u32)
+            The current back-to-back count.
+
+        Returns:
+        --------
+        int (size_t)
+            The number of points earned.
+        """
+        pass
